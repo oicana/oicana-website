9a3e4ea597ad91b0670af64b206a5098236fb925
Niklas Eicker <git@nikl.me>
2026-05-06 23:15:04 +0200

    More in beta post

--- src/starlightRouteData.ts
+++ src/starlightRouteData.ts
@@ -10,4 +10,14 @@ export const onRequest = defineRouteMiddleware((context) => {
 	if (sidebarlessPages.has(context.locals.starlightRoute.id)) {
 		context.locals.starlightRoute.hasSidebar = false;
 	}
+
+	const titleTag = context.locals.starlightRoute.head.find(
+		(tag) => tag.tag === "title",
+	);
+	if (titleTag?.content) {
+		const parts = titleTag.content.split(" | ");
+		if (parts.length === 2 && parts[0] === parts[1]) {
+			titleTag.content = parts[0];
+		}
+	}
 });
